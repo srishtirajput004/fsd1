67987882315f027e243ea372dcf4cc2f023cd83a
srishtirajput004 <rajlutsrishti@gmail.com>
2024-10-17 13:08:26 +0530

    done

--- js_work/index.js
+++ js_work/index.js
@@ -78,7 +78,59 @@
 //    let data1=selectlang("c");
 //    console.log(data1);
    
-console.log("hello world");
-let parent=document.getElementsByClassName("parent");
-console.log(parent);
-parent[0].innerHTML="<h2 style=color:red>DATA HAS CHANGED</h2>";
+// console.log("hello world");
+// // parent[0].innerText="hello js";
+// // parent[0].innerHTML="<h2 style=color:red>DATA HAS CHANGED</h2>";
+// const h1=document.createElement("h1");
+// console.log(h1);
+// h1.innerText="abes engineering college";
+// console.log(h1);
+// h1.style.backgroundColor="bisque";
+// h1.style.color="black";
+// h1.style.padding="20px";
+// h1.style.marginLeft="300px";
+// parent[0].appendChild(h1);
+
+// const img=document.createElement("img");
+// img.src="../images/ama logo.png";
+// console.log(img);
+// img.setAttribute("height","300");
+// img.setAttribute("width","500");
+// parent[0].appendChild(img);
+// parent[0].removeChild(img);
+
+// function getdata(){
+//     console.log("hi, inside getdata function");
+//     parent[0].innerHTML="<h2 style=color:red>hi, event handled </h2>";
+// }
+
+// const button=document.getElementsByClassName("btn");
+// console.log(button);
+// button[0].addEventListener("click",getdata);
+
+// const promise=new Promise((resolve,reject)=>{
+//     let a=12;
+//     if(a>10){
+//         resolve("accepted");
+//     }
+//     else{
+//         reject("not accepted");
+//     }
+
+// })
+// promise.then((msg)=>{console.log(msg)})
+// .catch((error)=>{console.log(error)})
+// .finally(()=>{console.log("all resources are closed")});
+
+// const promise2=new Promise((resolve,reject)=>{
+//     resolve({name:"srishti", Stack:"mern stack"});
+// })
+// promise2.then((data)=>{console.log("hi "+data.name)})
+// .catch(x=>{console.log(x)});
+
+const response=fetch("https://dummyjson.com/products");
+response.then((data)=>{console.log(data);
+    data.json().then((res)=>{
+        console.log(res);
+    })
+})
